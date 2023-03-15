18eafab2ea0d1c623ea5c15de278ee044396600e
Amit Khatri <amit.vkhatri@gmail.com>
2023-03-15 11:20:51 -0500

    added bitwise operators to Scraps

---
+++ Scraps/bitwise_operators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    unsigned int A = 13; // 0000 1101
+    unsigned int B = 60; // 0011 1100
+    unsigned int C;
+
+    C = A & B; // 0000 1100 =  12
+
+    printf("A is %d\n",A);
+    printf("B is %d\n\n",B);
+    
+    printf("A AND(&) B is %d\n",C);
+
+    C = A | B; // 0011 1101
+
+    printf("A OR(|) B is %d\n",C);
+
+    return 0;
+}
+
+    
+
+
